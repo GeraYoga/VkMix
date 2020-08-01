4117d279eb983c8404d5023e422ae7d538f3b904
GY <58561582+GeraYoga@users.noreply.github.com>
2020-08-01 23:43:52 +0300

    Add files via upload

---
+++ ATS.VkMix/RequestBuilder.cs
@@ -0,0 +1,90 @@
+﻿namespace ATS.VkMix
+{
+    public class RequestBuilder
+    {
+        private readonly VkMixRequest _vkMixRequest;
+
+        public RequestBuilder()
+        {
+            _vkMixRequest = new VkMixRequest();
+        }
+        
+        public RequestBuilder SetApiKey(string key)
+        {
+            _vkMixRequest.ApiKey = key;
+            return this;
+        }
+
+        public RequestBuilder SetTaskType(string type)
+        {
+            _vkMixRequest.RequestType = type;
+            return this;
+        }
+        
+        public RequestBuilder SetNetwork(string network)
+        {
+            _vkMixRequest.Network = network;
+            return this;
+        }
+        
+        public RequestBuilder SetSection(string selection)
+        {
+            _vkMixRequest.Section = selection;
+            return this;
+        }
+        
+        public RequestBuilder SetLink(string link)
+        {
+            _vkMixRequest.Link = link;
+            return this;
+        }
+        
+        public RequestBuilder SetCount(string count)
+        {
+            _vkMixRequest.Count = count;
+            return this;
+        }
+        
+        public RequestBuilder SetAmount(string amount)
+        {
+            _vkMixRequest.Amount = amount;
+            return this;
+        }
+        
+        public RequestBuilder SetComments(params string[] comments)
+        {
+            _vkMixRequest.Comments = comments;
+            return this;
+        }
+        
+        public RequestBuilder SetPoll(string poll)
+        {
+            _vkMixRequest.Poll = poll;
+            return this;
+        }
+        
+        public RequestBuilder SetHourlyLimit(string limit)
+        {
+            _vkMixRequest.HourlyLimit = limit;
+            return this;
+        }
+        
+        public RequestBuilder SetIds(params string[] ids)
+        {
+            _vkMixRequest.Ids = ids;
+            return this;
+        }
+
+        public RequestBuilder SetOffset(string offset)
+        {
+            _vkMixRequest.Offset = offset;
+            return this;
+        }
+
+        public VkMixRequest Build()
+        {
+            return _vkMixRequest;
+        }
+
+    }
+}
---
+++ ATS.VkMix/VkMixRequest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace ATS.VkMix
+{
+    public class VkMixRequest
+    {
+        public static RequestBuilder CreateBuilder()
+        {
+            return new RequestBuilder();
+        }
+        
+        public async Task<string> GetResponseAsync()
+        {
+            return await $"https://vkmix.com/api/2/{RequestType}"
+                .ToString()
+                .PostAsync(new FormUrlEncodedContent(CreateRequestBody()))
+                .ReceiveString();
+        }
+
+        private IEnumerable<KeyValuePair<string, string>> CreateRequestBody()
+        {
+            var collection = new List<KeyValuePair<string, string>>();
+            
+            collection.AddRange(new []
+            {
+                new KeyValuePair<string, string>("api_token", ApiKey),
+                new KeyValuePair<string, string>("network", Network), 
+                new KeyValuePair<string, string>("section", Section), 
+                new KeyValuePair<string, string>("link", Link), 
+                new KeyValuePair<string, string>("count", Count), 
+                new KeyValuePair<string, string>("amount", Amount),
+                new KeyValuePair<string, string>("poll", Poll),
+                new KeyValuePair<string, string>("offset", Offset),
+                new KeyValuePair<string, string>("hourly_limit", HourlyLimit),
+            });
+
+            if (Comments != null)
+            {
+                collection.AddRange(Comments.Select(comment => new KeyValuePair<string, string>("comments[]", comment)));
+            }
+
+            if (Ids != null)
+            {
+                collection.AddRange(Ids.Select(id => new KeyValuePair<string, string>("ids[]", id)));
+            }
+            
+            collection.RemoveAll(v => string.IsNullOrEmpty(v.Value));
+            
+            return collection;
+        }
+
+        public string ApiKey { get; set; }
+        public string RequestType { get; set; }
+        public string Network { get; set; }
+        public string Section { get; set; }
+        public string Link { get; set; }
+        public string Count { get; set; }
+        public string Amount { get; set; }
+        public string[] Comments  { get; set; }
+        public string Poll  { get; set; }
+        public string[] Ids { get; set; }
+        public string Offset { get; set; }
+        public string HourlyLimit { get; set; }
+    }
+}
